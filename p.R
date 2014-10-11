6659e8fba184f4b163a2d9a29ba67ce2c3ce5e7d
dennie allen <sysvrev0@gmail.com>
2014-10-11 00:59:01 -0600

    p.R : Calculate partitions of N using the Hardy-Ramanujan formula

---
+++ p.R
@@ -0,0 +1,11 @@
+p = function(N) {
+  # dennie allen "Sat Aug  9 21:40:36 2014"
+  # calc # of partitons of sums of N by 10's
+  # inspired by Ken Ono, Emory U, 'New Theories Reveal the Nature of Numbers'
+  df = data.frame(n = seq(0, N, by = 10))
+  P = data.frame()
+  df <- within(df, P <- sapply(df,function(n){floor(1/(4*n*sqrt(3)) * 		exp(pi*sqrt(2*n/3)))}))
+  colnames(df) <- c("n", "P(n)")
+  return(data.matrix(df))  # finally! data.matrix() displays colnames.
+}
+p(100)
